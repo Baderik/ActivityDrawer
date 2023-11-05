4e9d9973387935475322cd7385cb8e1df427dc68
baderik <baderikpavel@gmail.com>
2023-11-05 22:31:41 +1000

    Add basic functionality for drawing. Add comments. Create base templates for drawing.

--- main.py
+++ main.py
@@ -1,87 +1,191 @@
 import datetime as dt
-from subprocess import run
+from subprocess import run, PIPE
 from pathlib import Path
 
 
-class GitHubDrawer:
-    def __init__(self, directory: Path = Path(".")):
+def parse_draw(file_p: Path):
+    with open(file_p, encoding="utf-8") as inp:
+        data = inp.read().splitlines()
+    data = [tuple(map(int, tuple(line))) for line in data]
+    return data
+
+
+class Drawer:
+    def __init__(self, directory: Path = Path("."), branch="surface",
+                 remote_name="origin", remote_url=None):
         self.directory = directory.absolute()
-        self._today = dt.datetime.now()
-        self.first = self._today - dt.timedelta(days=364)
-        self.first -= dt.timedelta(days=(self.first.weekday() + 1) % 7)
+        self.surface_start_day = dt.datetime.now() - dt.timedelta(days=364)
+        self.surface_start_day -= dt.timedelta(days=(self.surface_start_day.weekday() + 1) % 7)
+        self.branch = branch
+        self.r_name = remote_name
+        self.r_url = remote_url
 
-    @staticmethod
-    def _gen_msg(date: dt.datetime) -> str:
-        return dt.datetime.now().strftime(
-            f'{date.strftime("Commit time: %Y.%m.%d %H:%M:%S")}\nReal time: %Y.%m.%d %H:%M:%S')
+        self.prepare_git()
+        self.prepare_branch()
 
-    def get_day(self, day_from_start: int) -> dt.datetime:
-        if day_from_start < 0:
-            raise ValueError(f"Day from start of dashboard must be non negative, but found: {day_from_start}")
-        return self.first + dt.timedelta(days=day_from_start)
+    def git(self, cmds: list[str], pipe_out: bool = True, *args, **kwargs):
+        """Run git command in right directory."""
+        if pipe_out:
+            return run(["git", "-C", self.directory] + cmds,
+                       stdout=PIPE, stderr=PIPE, text=True, *args, **kwargs)
+        return run(["git", "-C", self.directory] + cmds, *args, **kwargs)
 
-    def get_day_by_week_and_day(self, week_from_start: int, day_in_week: int):
-        if not (0 <= week_from_start < 53):
-            raise ValueError(f"Week from start must be in range(0, 53), but found: {week_from_start}")
-        if not (0 <= day_in_week < 7):
-            raise ValueError(f"Day of week must be in range(0, 7), but found: {day_in_week}")
+    def prepare_git(self):
+        """
+        Check that there is git in the directory.
+        Аnd creates a repository if it does not exist.
+        """
 
-        return self.get_day(week_from_start * 7 + day_in_week)
+        def check_exits_git():
+            """Check that git is installed."""
+            try:
+                self.git([])
+            except OSError:
+                raise Exception("You need a git")
 
-    def contribute(self, date: dt.datetime, file_name="Commit.md", msg: str = None):
+        def check_exist_repository():
+            """Check that git repository exists in directory."""
+            r = self.git(["status"])
+            # TODO: Add log
+            if r.returncode:
+                return False
+            return True
+
+        def create_rep():
+            """Create a git repository in directory"""
+            self.git(["init", "-b", self.branch], pipe_out=False)
+
+        check_exits_git()
+        if check_exist_repository():
+            return
+        create_rep()
+
+    def prepare_branch(self):
+        """Check that the branch exists and creates it if not."""
+
+        def branch_list():
+            """Get list of repository branches."""
+            return self.git(["branch"]).stdout.splitlines()
+
+        def parse_current(branches):
+            """Get name of current branch."""
+            for b in branches:
+                if b[0] == "*":
+                    return b.lstrip("*").lstrip()
+
+        def check_exist(branches):
+            """Check that the required branch exists."""
+            for b in branches:
+                if b.lstrip == self.branch:
+                    return True
+            return False
+
+        def switch2current():
+            """Switch repository to the required branch."""
+            return self.git(["checkout", self.branch])
+
+        def create_and_switch():
+            """Create the required branch and switch on it."""
+            return self.git(["checkout", "-b", self.branch])
+
+        b_list = branch_list()
+        if parse_current(b_list) == self.branch:
+            return
+
+        if check_exist(b_list):
+            switch2current()
+        else:
+            create_and_switch()
+
+    def prepare_remote(self):
+        """Check that the remote repository is connected and try to connect it."""
+        def check_exist_remote():
+            r = self.git(["remote", "show", self.r_name])
+            # TODO: Add log
+            if r.returncode:
+                return False
+            return True
+
+        def connect_remote():
+            self.git(["remote", "add", self.r_name, f'"{self.r_url}"'])
+
+        if check_exist_remote():
+            return
+        if self.r_url is None:
+            raise Exception("The url of remote repository is not set.")
+        connect_remote()
+
+    def get_day_by_week_and_day(self, week_from_start: int, day: int):
+        return self.surface_start_day + dt.timedelta(weeks=week_from_start, days=day)
+
+    def commit(self, date: dt.datetime, msg: str, file_name="README.md"):
         def date4git() -> str:
             return date.ctime()
 
-        if msg is None:
-            msg = self._gen_msg(date)
-
         with open(self.directory / file_name, "w") as out:
             print(msg, file=out)
-        run(["git", "-C", self.directory, "add", file_name])
-        run(["git", "-C", self.directory, "commit", "-m", msg, "--date", date4git()])
 
-    def draw_by_day(self, day: int, depth: int = 1):
-        if not (0 <= depth < 4):
-            raise ValueError()
+        self.git(["add", file_name])
+        self.git(["commit", "-m", date.strftime("%Y.%m.%d %H:%M:%S"), "--date", date4git()], pipe_out=False)
 
-        day = self.get_day(day)
+    def fill_cell(self, x: int, y: int, depth: int = 1):
+        """Fill cell of activity bar."""
+        def gen_msg(date: dt.datetime, d: int) -> str:
+            return (
+                f"This is an empty repository created specifically for drawing by GitHub activity.\n"
+                f"{dt.datetime.now().strftime('Current commit time: %Y.%m.%d %H:%M:%S')}\n"
+                f"{date.strftime('Recorded commit time: %Y.%m.%d %H:%M:%S')}({d})\n"
+                f"Look at: github.com/baderik/ActivityDrawer")
+
+        day = self.get_day_by_week_and_day(week_from_start=x, day=y)
         for i in range(depth):
-            self.contribute(day, msg=f"{self._gen_msg(day)}: {i}")
+            self.commit(day, msg=gen_msg(day, i))
 
-    def draw_by_index(self, x: int, y: int, depth: int = 1):
-        day = self.get_day_by_week_and_day(week_from_start=x, day_in_week=y)
-        for i in range(depth):
-            self.contribute(day, msg=f"{self._gen_msg(day)}: {i}")
+    def push(self, force: bool = True):
+        """Prepare remote and push new surface."""
+        self.prepare_remote()
+        self.git(["push", self.r_name, self.branch, "--force" if force else ""])
 
-    def push(self, force: bool = False):
-        run(["git", "-C", self.directory, "push", "--force" if force else ""])
+    def move_branch_commits(self, days: int = 7):
+        """Move all commits forward a specified number of days."""
+        delta_seconds = 86400 * days
+        update_command = (
+                "   date_str = commit.author_date.decode('utf-8')\n"
+                "   seconds, timezone = date_str.split()\n"
+                "   new_date = f'{int(seconds) + " + str(delta_seconds) + "} {timezone}'\n"
+                "   commit.author_date = new_date.encode('utf-8')")
+        self.git(["filter-repo", "--commit-callback", update_command, "--force"])
 
-    def update(self, days: int = 1):
+    def move_surface(self, weeks: int = 1):
+        """Move the old surface for a week."""
+        self.move_branch_commits(weeks * 7)
+
+    def reset_current_branch(self) -> None:
+        """Reset all commits of branch."""
+        # TODO: Check that it won't take down other branches... I'm not sure)
+        first_commit = self.git(["rev-list", "--max-parents=0", "HEAD"]).stdout.strip()
+        self.git(["reset", "--hard", first_commit], pipe_out=False)
+
+    def reset_surface(self):
+        """Reset surface by reset all commits branch."""
+        self.reset_current_branch()
+
+    def draw(self, mtx, shift: int = 0):
         """
-        git filter-branch --env-filter '
-        COMMIT_DATE=$(git log $GIT_COMMIT -n1 --format=%aD);
-        NEW_DATE=$(date -d "$COMMIT_DATE+1 day" -R);
-        GIT_COMMITTER_DATE="$NEW_DATE"
-        export GIT_COMMITTER_DATE
-        GIT_AUTHOR_DATE="$NEW_DATE"
-        export GIT_AUTHOR_DATE
-        ' SHA..HEAD
+        Draw a given matrix of the number of commits with a shift of the start week.
         """
+        if not mtx:
+            return
+        for x in range(len(mtx[0])):
+            for y in range(len(mtx)):
+                self.fill_cell(x + shift, y, int(mtx[y][x]))
 
 
 if __name__ == '__main__':
-    d = Path(r"D:\Projects\PycharmProjects\res")
-    g = GitHubDrawer(d)
-    # g.draw_by_day(10, 3)
-    with open("test.txt", encoding="utf-8") as inp:
-        data = inp.read().splitlines()
-
-    start_week = 4
-    for x in range(len(data[0])):
-        for y in range(len(data)):
-            g.draw_by_index(x + start_week, y, int(data[y][x]))
+    d = Path(r"D:\Projects\surface")
+    g = Drawer(d, remote_name=r"https://github.com/Baderik/ActivitySurface")
+    g.reset_surface()
+    f = Path(r"templates/dnk.txt")
+    m = parse_draw(f)
+    g.draw(m)
     g.push()
-
-    # print(g.first)
-    # print(g.first.weekday())
-    # print(dt.date(2023, 11, 5).weekday())
